5b27041074c43c67b430c4b2dbf56f45e2d5e9e1
tbonza <tylers.pile@gmail.com>
2019-01-19 15:38:23 -0500

    doing something I've done before

---
+++ 01-data-acqu/github/src/main/java/com/tbonzer/ds6050/github/GithubApplication.java
@@ -0,0 +1,14 @@
+package com.tbonzer.ds6050.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApplication.class, args);
+	}
+
+}
+
