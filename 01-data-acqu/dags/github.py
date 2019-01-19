7c06c914653dcbdb09f5876e2734a308b8e65d49
tbonza <tylers.pile@gmail.com>
2019-01-19 15:39:04 -0500

    cleaning up

--- 01-data-acqu/dags/github.py
+++
@@ -1,5 +0,0 @@
-""" Solution for DS 6050 Assignment 1
-
-http://janvitek.org/events/NEU/6050/a1.html
-"""
-
--- 01-data-acqu/worker0/client.py
+++
@@ -1,4 +0,0 @@
-""" Example client, this will eventually get moved to a DAG on airflow.
-
-Right now I'm just concerned with setting up a secure API on AWS.
-"""
