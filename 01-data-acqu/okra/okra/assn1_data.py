121cb25a0ef3a036578abeb2be11974f8a0925be
tbonza <tylers.pile@gmail.com>
2019-01-22 00:51:26 -0500

    strategy for parsing commits csv, see #4

--- 01-data-acqu/okra/okra/assn1_data.py
+++ 01-data-acqu/okra/okra/assn1_data.py
@@ -4,14 +4,17 @@ References:
    Assignment 1: "http://janvitek.org/events/NEU/6050/a1.html"
    Git log formatting: "https://git-scm.com/docs/pretty-formats"
 """
+import csv
 import logging
 import os
 import subprocess
 
+from okra.protobuf.assn1_pb2 import Commit, Message, File
+
 logger = logging.getLogger(__name__)
 
 
-def parse_commits_csv() -> str:
+def parse_commits(rpath: str):
     """ 
 
     commits.csv collects basic information about 
@@ -26,18 +29,55 @@ def parse_commits_csv() -> str:
     committer timestamp
     """
     c1 = ["git", "log",
-          """--pretty=format:'"%H","%an","%ae","%ad","%cn","%ce","%ct"'"""]
-    res = subprocess.run(c1, capture_output=True)
+          "--pretty=%H^|^%an^|^%ae^|^%ad^|^%cn^|^%ce^|^%ct"]
+    res = subprocess.run(c1, cwd=rpath, capture_output=True)
 
     if res.returncode == 0:
         logger.info("SUCCESS -- extracted commits_csv info")
-        return res.stdout
 
+        rows = res.stdout.splitlines()
+
+        for row_num, row in enumerate(rows):
+
+            items = row.decode('utf-8', 'ignore').split("^|^")
+
+            if len(items) == 7:
+                commit = Commit()
+
+                commit.hash_val = items[0]
+                commit.author = items[1]
+                commit.author_email = items[2]
+                commit.author_timestamp = items[3]
+                commit.committer = items[4]
+                commit.committer_email = items[5]
+                commit.committer_timestamp = items[6]
+                
+                yield commit
+
+            else:
+                print(len(items))
+                print(items)
+                logger.error("Issue with row {}, repo '{}'".\
+                             format(row_num, rpath))            
     else:
         logger.error("FAIL -- unable to extract commits_csv")
-        return ""
 
-def parse_messages_csv():
+def write_line_commits(parsed_commits):
+    """ Generate a line for each git commit message. """
+    for commit in parsed_commits:
+
+        row = [
+            commit.hash_val,
+            commit.author,
+            commit.author_email,
+            commit.author_timestamp,
+            commit.committer,
+            commit.committer_email,
+            commit.committer_timestamp,
+        ]
+        yield row
+
+def parse_messages(rpath: str):
     """
 
     messages.csv collects commit messages and their 
@@ -48,8 +88,8 @@ def parse_messages_csv():
     message
     """
     c1 = ["git", "log",
-          """--pretty=format:'"%H","%s","%b"'"""]
-    res = subprocess.run(c1, capture_output=True)
+          "--pretty=%H^|^%s^|^%b"]
+    res = subprocess.run(c1, cwd=rpath, capture_output=True)
 
     if res.returncode == 0:
         logger.info("SUCCESS -- extracted messages_csv info")
@@ -59,7 +99,7 @@ def parse_messages_csv():
         logger.error("FAIL -- unable to extract messages_csv")
         return ""
 
-def parse_files_csv():
+def parse_files(rpath: str):
     """
 
     files.csv informs which files were modified by 
@@ -73,7 +113,7 @@ def parse_files_csv():
     c1 = ["git", "log",
           '--pretty=format:"^|^%n%H"',
           '--numstat']
-    res = subprocess.run(c1, capture_output=True)
+    res = subprocess.run(c1, cwd=rpath, capture_output=True)
 
     if res.returncode == 0:
         logger.info("SUCCESS -- extracted files_csv info")
@@ -83,8 +123,49 @@ def parse_files_csv():
         logger.error("FAIL -- unable to extract files_csv")
         return ""
 
-def parse_assn1_data():
-    pass
+def write_bulk_csv(fpath: str, dirpath: str, repos:list, write_line,
+                   delim=",", quotechar='"'):
+    """ Write a bulk csv file.
+
+    This is honestly going to get corrupted no matter what
+    so I'm just doing this because the assignment requests 
+    it.
+
+    :param fpath: file path to csv output file
+    :param dirpath: directory path containing git repos
+    :param repos: list of repo names
+    :param write_line: function, used to write line for 
+        a specific row type.
+    :return: writes csv file to disk
+    :rtype: None
+    """
+    row_count = 0
+    try:
+        logger.info("STARTED writing csv file: {}".format(fpath))
+        with open(fpath, "w") as outfile:
+            writer = csv.writer(outfile, delimiter=delim,
+                                quotechar=quotechar,
+                                quoting=csv.QUOTE_MINIMAL)
+            
+            for row in write_line(repos, dirpath):
+
+                try:
+                    writer.writerow(row)
+
+                except Exception as exc:
+                    logger.exception(exc)
+
+                if row_count % 10000 == 0:
+                    logger.info("Wrote {} rows to {}".\
+                                format(row_count, fpath))
+
+                row_count += 1
+
+        logger.info("Wrote {} rows to {}".format(row_count, fpath))
+        logger.info("FINISHED writing csv file: {}".format(fpath))
+
+    except Exception as exc:
+        logger.exception(exc)
 
 def extract_data_main(fpath: str, dirpath: str):
     pass
--- 01-data-acqu/okra/setup.py
+++ 01-data-acqu/okra/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='okra',
-    version='0.3dev',
-    packages=['okra',],
+    version='0.4dev',
+    packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
+        'protobuf3',
     ],
     scripts=['bin/assn1'],
     setup_requires=['pytest-runner'],
