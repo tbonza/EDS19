e984df5488411117ae88cab7a60151f14bbba0ef
tbonza <tylers.pile@gmail.com>
2019-01-22 02:35:13 -0500

    data extraction currently running, see #4, see #5, see #6

--- 01-data-acqu/okra/setup.py
+++ 01-data-acqu/okra/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='okra',
-    version='0.4dev',
-    packages=find_packages(),
+    version='0.4dev1',
+    packages=["okra", "okra/protobuf"],
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
-        'protobuf3',
+        'protobuf==3.6.1',
     ],
     scripts=['bin/assn1'],
     setup_requires=['pytest-runner'],
