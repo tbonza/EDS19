4e00d5c4206362ac938ecb64a66fa612e0ed8c9d
tbonza <tylers.pile@gmail.com>
2019-01-24 20:29:00 -0500

    still doing tutorial

--- project/ui/README.md
+++ project/ui/README.md
@@ -4,6 +4,12 @@ User interface is called Highland. The front end is
 in Angular, Highland, and back end, Highland Server, 
 uses the Spring framework.
 
-**Next:**
 
-* [Loading a Dynamic Resource from Angular](Loading a Dynamic Resource from Angular)
+## Servers
+
+* Highland-Server
+* Resource
+
+## Clients
+
+* Highland
--- project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
+++ project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
@@ -21,14 +21,6 @@ import org.springframework.security.config.annotation.web.
 @RestController
 public class UiApplication {
 
-    @RequestMapping("/resource")
-    public Map<String,Object> home() {
-	Map<String,Object> model = new HashMap<String,Object>();
-	model.put("id", UUID.randomUUID().toString());
-	model.put("content", "Hello World");
-	return model;
-    }
-
     public static void main(String[] args) {
 	SpringApplication.run(UiApplication.class, args);
     }
