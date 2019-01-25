0ea44c4669e3d72ca30a998e89682c9eb1d34050
tbonza <tylers.pile@gmail.com>
2019-01-24 20:04:15 -0500

    working through tutorial

--- project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
+++ project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
@@ -6,8 +6,16 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.
+    builders.HttpSecurity;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.config.annotation.web.
+    configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
@@ -23,6 +31,27 @@ public class UiApplication {
 
     public static void main(String[] args) {
 	SpringApplication.run(UiApplication.class, args);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.IGNORED_ORDER)
+    protected static class SecurityConfiguration
+	extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http
+		.httpBasic()
+		.and()
+		.authorizeRequests()
+		  .antMatchers("/index.html", "/", "/home", "/login")
+		  .permitAll()
+		  .anyRequest().authenticated()
+		  .and()
+		.csrf()
+		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	    // @formatter: on
+	}
     }
 }
 
--- project/ui/highland/src/app/app.component.ts
+++ project/ui/highland/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,5 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'highland';
-    greeting = {'id': 'XXX', 'content': 'Hello World'};
+    constructor(private app: AppService, private http: HttpClient,
+		private router: Router){
+	this.app.authenticate(undefined, undefined);
+    }
+    logout() {
+	this.http.post('logout', {}).pipe(
+	    finalize(() => {
+	    this.app.authenticated = false;
+	    this.router.navigateByUrl('/login');
+	    })).subscribe()
+    }
 }
