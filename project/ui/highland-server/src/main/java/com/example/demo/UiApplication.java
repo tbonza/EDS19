5140512d4283b25771bf456218ffa0a003d06d6d
tbonza <tylers.pile@gmail.com>
2019-01-27 00:16:07 -0500

    finished the tutorial

--- project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
+++ project/ui/highland-server/src/main/java/com/example/demo/UiApplication.java
@@ -1,24 +1,29 @@
 package com.example.demo;
 
-import java.util.HashMap;
+import java.security.Principal;
+import java.util.Collections;
 import java.util.Map;
-import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.
     builders.HttpSecurity;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.config.annotation.web.
     configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@RestController
+@Controller
+@EnableZuulProxy
 public class UiApplication {
 
     public static void main(String[] args) {
@@ -32,18 +37,25 @@ public class UiApplication {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+	    // @formatter:off
 	    http
-		.httpBasic()
-		.and()
+		.httpBasic().and()
+		.logout().and()
 		.authorizeRequests()
 		  .antMatchers("/index.html", "/", "/home", "/login")
 		  .permitAll()
-		  .anyRequest().authenticated()
-		  .and()
+		  .anyRequest().authenticated().and()
 		.csrf()
-		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		  .csrfTokenRepository(CookieCsrfTokenRepository
+				     .withHttpOnlyFalse());
 	    // @formatter: on
 	}
+    }
+
+    @GetMapping("/token")
+    @ResponseBody
+    public Map<String,String> token(HttpSession session){
+	return Collections.singletonMap("token", session.getId());
     }
 }
 
--- project/ui/resource/src/main/java/com/example/demo/ResourceApplication.java
+++ project/ui/resource/src/main/java/com/example/demo/ResourceApplication.java
@@ -4,6 +4,12 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation
+    .web.builders.HttpSecurity;
+import org.springframework.security.config.annotation
+    .web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResourceApplication {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	http.cors().and().authorizeRequests()
+	    .andyRequest().authenticated();
+    }
+
     @RequestMapping("/")
-    @CrossOrigin(origins="*", maxAge=3600)
+    @CrossOrigin(origins="*", maxAge=3600,
+		 allowedHeaders={"x-auth-token", "x-requested-with",
+				 "x-xsrf-token"})
     public Message home(){
 	return new Message("Hello World");
+    }
+
+    @Bean
+    HeaderHttpSessionStrategy sessionStrategy(){
+	return new HeaderHttpSessionStrategy();
     }
     
     public static void main(String[] args) {
