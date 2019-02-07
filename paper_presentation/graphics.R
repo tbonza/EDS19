9ba52fe67d63aae1f9c62dadda6d946e3eb251a5
tbonza <tylers.pile@gmail.com>
2019-02-07 17:34:41 -0500

    finished figures for presentation, resolves #20

--- paper_presentation/graphics.R
+++ paper_presentation/graphics.R
@@ -42,3 +42,40 @@ f1 <- ggplot(tg5, aes(c_peril, r_peril)) +
           axis.title.y=element_blank(),
           text=element_text(size=16, face="bold"))
 ggsave("imgs/table5_revamp.png", f1)
+
+# Redo Table 2
+
+tg2 <- tibble(
+    `Category of use` = factor(c("Software development", "Experimental",
+                          "Storage", "Academic", "Web",
+                          "No longer accessible", "Empty")),
+    `Number of repositories` = c(275,53,36,31,25,11,3),
+    percentage = c(63.4, 12.2, 8.3, 7.1, 5.8, 2.5, 0.7)
+)
+
+f2 <- ggplot(tg2, aes(`Category of use`, `Number of repositories`)) +
+    geom_col(aes(fill=percentage)) + coord_flip() +
+    scale_fill_distiller(palette = "Spectral") +
+    theme(axis.text.y = element_text(angle=45)) +
+    labs(title="(Table 2) Number of repositories per type of use",
+         subtitle="Mutually exclusive categories for the manual inspection",
+         x="Number of Repositories", y="Category of Use")
+ggsave("imgs/table2_revamp.png", f2)
+
+
+# Table 3 redo
+
+tg3 <- tibble(
+    `Set` = c("Located on Sourceforge",
+              "Located on Bitbucket", "From subversion repos",
+              "From mercurial repos", "From CVS repos"),
+    `Projects` = c(117, 91, 622, 113, 55),
+    `Repos` = c(511, 249, 4966, 590, 212)
+)
+
+f3 <- ggplot(tg3, aes(`Set`, `Repos`, fill=`Projects`)) +
+    geom_col() + coord_flip() +
+    scale_fill_distiller(palette = "Spectral") +
+    theme(axis.text.y = element_text(angle=45)) +
+    labs(title="(Table 3) Repositories hosted on GitHub labeled as mirrors")
+ggsave("imgs/table3_revamp.png", f3)
