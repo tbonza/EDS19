dc56a76a9d2e02e54f3975053a412bd52f7f2746
tbonza <tylers.pile@gmail.com>
2019-02-21 00:21:06 -0500

    removed pyyaml dependency due to security vulnerability, see #2

--- okra/setup.py
+++ okra/setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='okra',
-    version='0.8dev5',
-    packages=["okra", "okra/protobuf", "okra/playbooks"],
+    version='0.8dev6',
+    packages=["okra", "okra/protobuf"],
     license='MIT',
     long_description=open('README.md').read(),
     install_requires=[
         'boto3==1.9.93',
         'protobuf==3.6.1',
         'sqlalchemy==1.2.17',
-        'pyyaml==3.13',
         'redis==3.2.0',
     ],
     scripts=['bin/okra'],
