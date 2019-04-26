703243b8f0e17694392f8353a4ec5a78c8479330
tbonza <tylers.pile@gmail.com>
2019-04-26 04:04:58 -0400

    updated sqlalchemy vulnerability, this package resolves #9, resolves #23, resolves #24"

--- okra/setup.py
+++ okra/setup.py
@@ -11,7 +11,7 @@ setup(
         'pandas==0.24.2',
         'protobuf==3.6.1',
         'pyarrow==0.13.0',
-        'sqlalchemy==1.2.17',
+        'sqlalchemy>=1.3.0',
         'redis==3.2.0',
     ],
     scripts=['bin/okra', 'bin/dokra'],
